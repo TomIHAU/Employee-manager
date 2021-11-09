62457c971b70de9f75cbf24b60d95974aaefc2ed
TauH <thaywood988@gmail.com>
2021-11-09 12:08:36 +1100

    basic seeds layout

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+
+    user: "root",
+
+    password: "rootroot",
+    database: "employeetracker_db",
+  },
+  console.log(`Connected to the employeetracker_db database.`)
+);
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

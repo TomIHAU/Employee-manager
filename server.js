fe3e3dcd8bc77f05c29b77825fb926bc396cf14c
TauH <thaywood988@gmail.com>
2021-11-09 14:13:18 +1100

    not using xpress..

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 const PORT = process.env.port || 3001;
@@ -11,15 +12,19 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: "localhost",
-
     user: "root",
-
     password: "rootroot",
     database: "employeetracker_db",
   },
   console.log(`Connected to the employeetracker_db database.`)
 );
-
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({ message: "success", data: body });
+});
 app.use((req, res) => {
   res.status(404).end();
 });
